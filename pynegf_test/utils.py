f440faf57817a2747089ba0bf5e530e342251b5e
Gabriele Penazzi <g.penazzi@gmail.com>
2020-03-13 09:21:06 +0100

    Linter

--- pynegf_test/utils.py
+++ pynegf_test/utils.py
@@ -16,17 +16,17 @@ def orthogonal_linear_chain(nsites=100, contact_size=20, coupling=1.0):
         nsites (int): the number of sites.
         coupling (complex): the hopping matrix element.
     """
-    if (contact_size >= nsites / 2):
+    if contact_size >= nsites / 2:
         raise ValueError("Contacts are too large")
 
     mat = numpy.zeros(shape=(nsites, nsites), dtype='complex128')
 
-    for ii in range(nsites - contact_size):
-        mat[ii, ii - 1] = numpy.conj(coupling)
-        mat[ii - 1, ii] = coupling
-    for ii in range(nsites - contact_size, nsites):
-        mat[ii, ii - 1] = numpy.conj(coupling)
-        mat[ii - 1, ii] = coupling
+    for i in range(nsites - contact_size):
+        mat[i, i - 1] = numpy.conj(coupling)
+        mat[i - 1, i] = coupling
+    for i in range(nsites - contact_size, nsites):
+        mat[i, i - 1] = numpy.conj(coupling)
+        mat[i - 1, i] = coupling
     mat[80, 0] = numpy.conj(coupling)
     mat[0, 80] = coupling
 
