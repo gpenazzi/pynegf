420194b12961d00776ee3d2278f584fca5c2a697
Gabriele Penazzi <g.penazzi@gmail.com>
2020-05-03 15:03:16 +0200

    Take blas lapack from numpy and make the settings nicer

--- pynegf/__init__.py
+++ pynegf/__init__.py
@@ -1,6 +1,8 @@
+from ctypes import util
+import collections
 import ctypes
-import ctypes.util
 import logging
+import numpy
 import typing
 
 # Package imports.
@@ -19,6 +21,38 @@ class Settings(dict):
         self.update(self.defaults())
 
     @staticmethod
+    def _set_dependencies():
+        """
+        Set the list of dependencies linked at runtime. This includes
+        libnegf, blas and lapack.
+        """
+        # Add libnegf.
+        path = util.find_library('negf')
+        dependencies = {}
+        dependencies['negf'] = {
+            'paths': [path]}
+
+        # Add blas and lapack from numpy. The first found is used.
+        numpy_config = numpy.__config__
+        blas_info = [
+            x for x in dir(numpy_config) if 'blas' in x and '_info' in x][0]
+        if blas_info:
+            blas_info = getattr(numpy_config, blas_info)
+            dependencies['blas'] = {
+                'paths': [
+                    util.find_library(x) for x in blas_info['libraries']]}
+
+        lapack_info = [
+            x for x in dir(numpy_config) if 'lapack' in x and '_info' in x][0]
+        if lapack_info:
+            lapack_info = getattr(numpy_config, lapack_info)
+            dependencies['lapack'] = {
+                'paths': [
+                    util.find_library(x) for x in lapack_info['libraries']]}
+
+        return dependencies
+
+    @staticmethod
     def defaults() -> typing.Dict[str, typing.Any]:
         """
         Returns a dictionary of default settings. The available settings are:
@@ -27,10 +61,9 @@ class Settings(dict):
         `blas_path`: path of libblas
         `lapack_path`: path of liblapack
         """
-        defaults = {}
-        # Populate the default paths.
-        for lib in ('negf', 'blas', 'lapack'):
-            defaults[lib + '_path'] = ctypes.util.find_library(lib)
+        defaults = {
+            'dependencies': Settings._set_dependencies()}
+        defaults['loglevel'] = logging.INFO
 
         return defaults
 
@@ -38,54 +71,57 @@ class Settings(dict):
 # Initialize global settings.
 settings = Settings()
 
-# Set log level temporarily hardcoded.
-logging.getLogger().setLevel(logging.INFO)
+# Inizialize runtime parameters.
+RuntimeEnvironment = collections.namedtuple(
+    'RuntimeEnvironment',
+    ['dependencies', 'logger'])
 
 
-def load_dependencies() -> typing.Dict[str, ctypes.CDLL]:
+def _load_dependencies() -> typing.Dict[str, ctypes.CDLL]:
     """
     Load at module level the ctypes dependencies.
 
     Returns:
         A dictionary containing the loaded library instances.
     """
-
+    global _dependencies
     # Load libraries
     error_message = (
-        "lib{0} was not found. Provide a path in pynegf.settings['{0}_path'] "
-        " and run >>>pynegf.dependencies = pynegf.load_dependencies()")
+        "Resource {0} was not found. Provide a list of paths in "
+        " pynegf.settings['dependencies']['paths'] "
+        " and run >>> pynegf.load_runtime_environment().")
 
+    dependencies = {}
     try:
-        blas_cdll = ctypes.CDLL(
-            settings['blas_path'],
-            mode=ctypes.RTLD_GLOBAL)
+        dependencies['negf'] = ctypes.CDLL(
+            settings['dependencies']['negf']['paths'][0])
     except OSError:
-        blas_cdll = None
-        logging.warning(error_message.format('blas'))
-
-    try:
-        lapack_cdll = ctypes.CDLL(
-            settings['lapack_path'],
-            mode=ctypes.RTLD_GLOBAL)
-    except OSError:
-        lapack_cdll = None
-        logging.warning(error_message.format('lapack'))
-
-    try:
-        negf_cdll = ctypes.CDLL(settings['negf_path'])
-    except OSError:
-        negf_cdll = None
         logging.warning(error_message.format('negf'))
 
-    dependencies = {
-        'negf': negf_cdll,
-        'blas': blas_cdll,
-        'lapack': lapack_cdll}
+    for key, val in [
+            x for x in settings['dependencies'].items() if x[0] != 'negf']:
+        if not val['paths']:
+            logging.warning(error_message.format('key'))
+        for path in val['paths']:
+            dependencies[key] = ctypes.CDLL(path, mode=ctypes.RTLD_GLOBAL)
 
     return dependencies
 
 
-dependencies = load_dependencies()
+def load_runtime_environment() -> RuntimeEnvironment:
+    """
+    Load runtime environment as specified in pynegf.settings.s
+    """
+    global runtime_environment
+    runtime_environment = RuntimeEnvironment(
+        dependencies=_load_dependencies(),
+        logger=logging.getLogger('pynegfLogger'))
+    runtime_environment.logger.setLevel(settings['loglevel'])
+
+    return runtime_environment
+
+
+runtime_environment = load_runtime_environment()
 
 
 def cdll_libnegf() -> ctypes.CDLL:
@@ -93,4 +129,4 @@ def cdll_libnegf() -> ctypes.CDLL:
     Return:
         The loaded CDLL object
     """
-    return dependencies['negf']
+    return runtime_environment.dependencies['negf']
